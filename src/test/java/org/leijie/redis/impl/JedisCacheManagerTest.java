ab2cefc04be670e5fd4d63f1d61a23b2afb4c8d7
leijie <leijie@lairen.com>
2017-07-12 15:12:28 +0800

    添加子模块

--- src/test/java/org/leijie/redis/impl/JedisCacheManagerTest.java
+++ src/test/java/org/leijie/redis/impl/JedisCacheManagerTest.java
@@ -5,7 +5,6 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
-import org.leijie.redis.impl.JedisCacheManager;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.leijie.Application;
