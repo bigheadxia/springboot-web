47b31a9d483f43731dc63f901362d6b8c9c759cb
leijie <leijie@lairen.com>
2017-07-12 09:37:51 +0800

    Initial commit

---
+++ src/main/java/org/tdcg/nio/NioTest.java
@@ -0,0 +1,93 @@
+package org.tdcg.nio;
+
+import java.io.FileInputStream;
+import java.nio.Buffer;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * Created with IDEA
+ * User: LEIJIE
+ * Date: 2017/7/10 0010
+ * Time: 16:47
+ */
+public class NioTest {
+
+    public static void main2(String[] args) {
+        // 分配新的int缓冲区，参数为缓冲区容量
+        // 新缓冲区的当前位置将为零，其界限(限制位置)将为其容量。它将具有一个底层实现数组，其数组偏移量将为零。
+        IntBuffer buffer = IntBuffer.allocate(8);
+
+        for (int i = 0; i < buffer.capacity(); ++i) {
+            int j = 2 * (i + 1);
+            // 将给定整数写入此缓冲区的当前位置，当前位置递增
+            buffer.put(j);
+        }
+
+        // 重设此缓冲区，将限制设置为当前位置，然后将当前位置设置为0
+        buffer.flip();
+
+        // 查看在当前位置和限制位置之间是否有元素
+        while (buffer.hasRemaining()) {
+            // 读取此缓冲区当前位置的整数，然后当前位置递增
+            int j = buffer.get();
+            System.out.print(j + "  ");
+        }
+    }
+
+    static public void main3( String args[] ) throws Exception {
+        FileInputStream fin = new FileInputStream("C:\\Users\\Administrator.EIT-20160604RCD\\Desktop\\test.txt");
+
+        // 获取通道
+        FileChannel fc = fin.getChannel();
+
+        // 创建缓冲区
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+        // 读取数据到缓冲区
+        fc.read(buffer);
+
+        buffer.flip();
+
+        while (buffer.remaining()>0) {
+            byte b = buffer.get();
+            System.out.print(((char)b));
+        }
+
+        fin.close();
+    }
+
+    public static void main(String args[]) throws Exception {
+        FileInputStream fin = new FileInputStream("C:\\Users\\Administrator.EIT-20160604RCD\\Desktop\\test.txt");
+        FileChannel fc = fin.getChannel();
+
+        ByteBuffer buffer = ByteBuffer.allocate(10);
+        output("初始化", buffer);
+
+        fc.read(buffer);
+        output("调用read()", buffer);
+
+        buffer.flip();
+        output("调用flip()", buffer);
+
+        while (buffer.remaining() > 0) {
+            byte b = buffer.get();
+            // System.out.print(((char)b));
+        }
+        output("调用get()", buffer);
+
+        buffer.clear();
+        output("调用clear()", buffer);
+
+        fin.close();
+    }
+
+    public static void output(String step, Buffer buffer) {
+        System.out.println(step + " : ");
+        System.out.print("capacity: " + buffer.capacity() + ", ");
+        System.out.print("position: " + buffer.position() + ", ");
+        System.out.println("limit: " + buffer.limit());
+        System.out.println();
+    }
+}
---
+++ src/main/java/org/tdcg/web/TestController.java
@@ -0,0 +1,11 @@
+package org.tdcg.web;
+
+/**
+ * Created with IDEA
+ * User: LEIJIE
+ * Date: 2017/7/12 0012
+ * Time: 09:29
+ */
+
+public class TestController {
+}
